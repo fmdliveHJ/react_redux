5d45ac5e209ee84ad22745474c50a52a588fc62a
hyeonjin <gdalove@naver.com>
2023-02-09 22:34:24 +0900

    리덕스 툴킷 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-function App() {
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const { logIn } = require("./actions/user");
+const userSlice = require("./reducers/userSlice");
+
+const App = () => {
+  const user = useSelector((state) => state.user);
+  const { list } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+
+  const onClick = useCallback(() => {
+    dispatch(
+      logIn({
+        id: "정현진",
+        password: "비밀번호",
+      })
+    );
+  }, []);
+
+  const onLogout = useCallback(() => {
+    dispatch(userSlice.actions.logOut());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user.isLoggingIn ? (
+        <div>로그인 중</div>
+      ) : user.data ? (
+        <div>{user.data.nickname}</div>
+      ) : (
+        "로그인 해주세요."
+      )}
+      {!user.data ? (
+        <button onClick={onClick}>로그인</button>
+      ) : (
+        <button onClick={onLogout}>로그아웃</button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
